f70aa2557238702df56cea669cb02bcb2ac4978f
darkieson <darkiesoncodesudio@gmail.com>
2025-05-27 18:47:54 +0100

    commit6

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import TemplateScreen from './screens/TemplateScreen';
-import RequestScreen from './screens/RequestScreen';
+import React from 'react';
+import HomeScreen from './screens/homescreen';
+import RequestScreen from './screens/requestscreen';
+import TemplateScreen from './screens/templatescreen';
 
 const Stack = createNativeStackNavigator();
 
