d2549e11d5632b4346a69982fb2c1eacf8daf911
darkieson <darkiesoncodesudio@gmail.com>
2025-05-31 11:36:04 +0100

    commit7

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,8 +1,8 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet } from 'react-native';
 
-import { Collapsible } from '@/components/Collapsible';
-import { ExternalLink } from '@/components/ExternalLink';
+import { Collapsible } from '../../components/Collapsible';
+import { ExternalLink } from '../../components/ExternalLink';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,9 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
-import HomeScreen from './screens/homescreen';
-import RequestScreen from './screens/requestscreen';
-import TemplateScreen from './screens/templatescreen';
-
+import HomeScreen from '../screens/HomeScreen';
+import TemplateScreen from '../screens/TemplateScreen';
+import RequestScreen from '../screens/requestscreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
